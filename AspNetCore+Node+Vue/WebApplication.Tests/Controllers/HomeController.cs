9ccc384e874a44139a67d1fca7f8d45de49b0fbd
clby <1413369390@qq.com>
2017-03-10 23:19:18 +0800

    Examples

---
+++ AspNetCore+Node+Vue/WebApplication.Tests/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+
+namespace WebApplication.Tests.Controllers
+{
+    public class HomeController : Controller
+    {
+        private INodeServices NodeServices;
+        public HomeController(INodeServices nodeServices)
+        {
+            NodeServices = nodeServices;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Test()
+        {
+            var result = await NodeServices.InvokeAsync<int>("./Node/addNumbers", 1, 2);
+            return Content("1 + 2 = " + result);
+
+
+            //return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetCore+Node+Vue/WebApplication.Tests/Node/boot-server.js
@@ -0,0 +1,22 @@
+﻿var prerendering = require('aspnet-prerendering');
+
+var Vue = require('vue')
+var app = new Vue({
+  render: function (h) {
+    return h('p', 'hello world')
+  }
+})
+
+var renderer = require('vue-server-renderer').createRenderer()
+
+
+module.exports = prerendering.createServerRenderer(function(params) {
+    return new Promise(function (resolve, reject) {
+		
+	    renderer.renderToString(app, function (error, result) {
+		  if (error) throw error		  
+			resolve({ html: result });
+		})
+		
+    });
+});
