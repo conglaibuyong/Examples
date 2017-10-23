533c2d295688baf4b6bab79adbb6e01dcf1f30ee
clby <1413369390@qq.com>
2017-10-23 23:08:00 +0800

    ufop-netcore-demo
    ufop-netcore-demo

---
+++ ufop-netcore-demo/build.cmd
@@ -0,0 +1,2 @@
+dotnet publish ufop-netcore-demo/ufop-netcore-demo.csproj -o obj/Docker/publish
+docker-compose build
---
+++ ufop-netcore-demo/ufop-netcore-demo/Controllers/HandlerController1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ufop_netcore_demo.Controllers
+{
+    [Route("Handler")]
+    public class HandlerController1 : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Json(new
+            {
+                v = "Get OK v2"
+            });
+        }
+
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return this.Json(new
+            {
+                v = "Post OK v2"
+            });
+        }
+
+    }
+}
